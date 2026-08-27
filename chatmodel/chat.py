e2fa5d6b91dfbea5d44a5a71fbfbcf2b87d1aef5
aakash301p <aakash301p@gmail.com>
2026-08-27 14:31:10 +0530

    small change

--- chatmodel/chat.py
+++ chatmodel/chat.py
@@ -1,7 +1,7 @@
-from pydantic import SecretStr
 from langchain_mistralai.chat_models import ChatMistralAI
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 load_dotenv()
 
@@ -9,5 +9,5 @@ client = ChatMistralAI(
     model_name="mistral-small-2506",
     temperature=0.9,
     max_tokens=512,
-    api_key=SecretStr(os.getenv("MISTRAL_API_KEY", "")),
+    api_key =os.getenv("MISTRAL_API_KEY") or st.secrets.get("MISTRAL_API_KEY")
 )
