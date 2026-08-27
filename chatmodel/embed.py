7bb9bc45151119ec11c88699d7b05d328c49c9ef
aakash301p <aakash301p@gmail.com>
2026-08-27 13:23:23 +0530

    small change

--- chatmodel/embed.py
+++ chatmodel/embed.py
@@ -2,7 +2,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
 
 from langchain_huggingface import HuggingFaceEmbeddings
-import app as st
+import streamlit as st
 
 @st.cache_resource
 def load_embeddings():
